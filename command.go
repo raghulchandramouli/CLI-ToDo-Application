5b685083ff6a16ec9dec49af0322dc5c8192f7dd
Raghul <raghulchandramouli@gmail.com>
2024-09-08 12:57:54 +0530

    done with main.go package

--- command.go
+++ command.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-    "flag"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type CmdFlags struct {
@@ -25,4 +29,41 @@ func NewCmdFlags() *CmdFlags {
 	flag.Parse()
 
 	return &cf
+}
+
+func (cf *CmdFlags) Execute(todos *Todos) {
+	switch {
+	case cf.List:
+		todos.print()
+
+	case cf.Add!= "":
+		todos.add(cf.Add)
+	
+	case cf.Edit!= "":
+		parts := strings.SplitN(cf.Edit, "", 2)
+		if len(parts) != 2 {
+			fmt.Println("Invalid edit command. Please use id:new_title")
+            os.Exit(1)
+        }
+
+		index, err := strconv.Atoi(parts[0])
+
+		if err != nil {
+			fmt.Println("Error parsing index. Please use a valid index.")
+			os.Exit(1)
+		}
+		
+		todos.edit(index, parts[1])
+
+	case cf.Toggle != -1:
+		todos.toggle(cf.Toggle)
+
+	
+	case cf.Del!= -1:
+		todos.delete(cf.Del)
+
+	default:
+		fmt.Println("Invalid command.")
+
+	}
 }

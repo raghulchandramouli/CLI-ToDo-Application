0b58649450e44e8414e7407da814d8bd33d486df
Raghul <raghulchandramouli@gmail.com>
2024-09-08 12:39:54 +0530

    NewCmDFlags function done

--- command.go
+++ command.go
@@ -12,10 +12,17 @@ type CmdFlags struct {
 	List bool
 }
 
-func NewCmdFLags() *CmdFlags {
+func NewCmdFlags() *CmdFlags {
 
 	cf := CmdFlags{}
 
 	flag.StringVar(&cf.Add, "add", "", "Add a new todo specify title.")
 	flag.StringVar(&cf.Edit, "Edit", "", "Edit a todo by index & specify a new title, id:new_title")
+	flag.IntVar(&cf.Del, "Del", -1, "Specify a ToDo Index to be deleted")
+	flag.IntVar(&cf.Toggle, "Toggle", -1, "Specify a ToDo Index to toggle")
+	flag.BoolVar(&cf.List, "List", false, "List all ToDos")
+
+	flag.Parse()
+
+	return &cf
 }
